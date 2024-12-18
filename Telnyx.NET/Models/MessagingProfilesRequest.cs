fe1db113fd06045fc747f4578151afd5b2204050
Vimal <vimal.kumar@spirerecoverysolutions.com>
2024-12-18 14:35:53 -0500

    Added List messaging profiles endpoint

---
+++ Telnyx.NET/Models/MessagingProfilesRequest.cs
@@ -0,0 +1,11 @@
+﻿using Telnyx.NET.Interfaces;
+
+namespace Telnyx.NET.Models
+{
+    public class MessagingProfilesRequest : ITelnyxRequest
+    {
+        public int? PageNumber { get; set; }
+        public int? PageSize { get; set; }
+        public string? NameFilter { get; set; }
+    }
+}
