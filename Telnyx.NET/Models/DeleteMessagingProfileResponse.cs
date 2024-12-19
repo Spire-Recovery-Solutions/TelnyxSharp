bfbdf0768add160e9268c9aa81e4e551d5a5953c
Vimal <vimal.kumar@spirerecoverysolutions.com>
2024-12-19 09:30:18 -0500

    Added Delete a messaging profile endpoint

---
+++ Telnyx.NET/Models/DeleteMessagingProfileResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Interfaces;
+
+namespace Telnyx.NET.Models
+{
+    public class DeleteMessagingProfileResponse : ITelnyxResponse
+    {
+        /// <summary>
+        /// Gets or sets the data for the updated messaging profile.
+        /// </summary>
+        [JsonPropertyName("data")]
+        public DeleteMessagingProfileData Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of errors, if any occurred during the API call.
+        /// </summary>
+        [JsonPropertyName("errors")]
+        public TelnyxError[]? Errors { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the data associated with the updated messaging profile.
+    /// Inherits properties from the base <see cref="MessagingProfileBase"/> class.
+    /// </summary>
+    public class DeleteMessagingProfileData : MessagingProfileBase
+    {
+    }
+}
