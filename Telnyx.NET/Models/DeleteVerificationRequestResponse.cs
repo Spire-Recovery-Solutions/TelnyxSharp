7cff6388f49aef0a2443e9117294117ab0c2a1f5
Vimal <vimal.kumar@spirerecoverysolutions.com>
2024-12-23 16:28:05 -0500

    Added Toll-free Verification endpoints

---
+++ Telnyx.NET/Models/DeleteVerificationRequestResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Interfaces;
+
+namespace Telnyx.NET.Models
+{
+    public class DeleteVerificationRequestResponse : ITelnyxResponse
+    {
+        /// <summary>
+        /// Represents any errors encountered during the API call.
+        /// </summary>
+        [JsonPropertyName("errors")]
+        public TelnyxError[]? Errors { get; set; }
+    }
+}
---
+++ Telnyx.NET/Models/GetVerificationRequestResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Interfaces;
+
+namespace Telnyx.NET.Models
+{
+    public class GetVerificationRequestResponse : BaseVerificationRequestResponse, ITelnyxResponse
+    {
+    }
+}
