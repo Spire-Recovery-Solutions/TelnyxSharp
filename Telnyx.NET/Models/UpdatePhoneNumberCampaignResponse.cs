4945095313c10b1ee775381841e4a762ca80639e
Vimal <vimal.kumar@spirerecoverysolutions.com>
2024-12-27 15:09:18 -0500

    Added Phone Number Campaign endpoints

---
+++ Telnyx.NET/Models/UpdatePhoneNumberCampaignResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Interfaces;
+
+namespace Telnyx.NET.Models
+{
+    public class UpdatePhoneNumberCampaignResponse : BasePhoneNumberCampaigns, ITelnyxResponse
+    {
+        /// <summary>
+        /// List of errors, if any occurred during the API call.
+        /// </summary>
+        [JsonPropertyName("errors")]
+        public TelnyxError[]? Errors { get; set; }
+    }
+}
