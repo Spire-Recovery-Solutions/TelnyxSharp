6f5fccf7994bfe1cfbcaf2eb680a13defcfc6938
Vimal <vimal.kumar@spirerecoverysolutions.com>
2024-12-30 11:19:43 -0500

    Added Shared Campaign endpoints

---
+++ Telnyx.NET/Models/GetSharedCampaignRecordResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Interfaces;
+
+namespace Telnyx.NET.Models
+{
+    public class GetSharedCampaignRecordResponse : BaseSharedCampaigns, ITelnyxResponse
+    {
+        /// <summary>
+        /// Represents any errors encountered during the retrieval of the auto-response setting.
+        /// </summary>
+        [JsonPropertyName("detail")]
+        public List<ValidationErrorDetail>? Detail { get; set; }
+
+    }
+}
