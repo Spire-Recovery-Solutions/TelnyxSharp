1b2aee807f2409a4459aff2caac088f9bbf6e5cd
Vimal <vimal.kumar@spirerecoverysolutions.com>
2024-12-30 17:14:46 -0500

    Added comments

--- Telnyx.NET/Enums/BrandRelationship.cs
+++ Telnyx.NET/Enums/BrandRelationship.cs
@@ -2,24 +2,45 @@
 
 namespace Telnyx.NET.Enums
 {
+    /// <summary>
+    /// Enum representing the possible brand relationships with Telnyx.
+    /// </summary>
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum BrandRelationship
     {
+        /// <summary>
+        /// Unknown brand relationship status.
+        /// </summary>
         [JsonPropertyName("UNKNOWN")]
         Unknown,
 
+        /// <summary>
+        /// Basic account relationship.
+        /// </summary>
         [JsonPropertyName("BASIC_ACCOUNT")]
         BasicAccount,
 
+        /// <summary>
+        /// Small account relationship.
+        /// </summary>
         [JsonPropertyName("SMALL_ACCOUNT")]
         SmallAccount,
 
+        /// <summary>
+        /// Medium account relationship.
+        /// </summary>
         [JsonPropertyName("MEDIUM_ACCOUNT")]
         MediumAccount,
 
+        /// <summary>
+        /// Large account relationship.
+        /// </summary>
         [JsonPropertyName("LARGE_ACCOUNT")]
         LargeAccount,
 
+        /// <summary>
+        /// Key account relationship.
+        /// </summary>
         [JsonPropertyName("KEY_ACCOUNT")]
         KeyAccount
     }
