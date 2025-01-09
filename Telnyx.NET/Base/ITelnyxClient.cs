c5b1d8ba73c6402ab013acf0b0cd8e8a4e2162e0
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-09 11:27:58 -0500

    Fix Naming

--- Telnyx.NET/Base/ITelnyxClient.cs
+++ Telnyx.NET/Base/ITelnyxClient.cs
@@ -26,23 +26,23 @@ namespace Telnyx.NET.Base
         /// Gets the operations for 10DLC services.
         /// This includes managing 10DLC campaigns and configuring phone numbers for 10DLC services.
         /// </summary>
-        ITenDlcOperations TenDlcOperations { get; }
+        ITenDlcOperations TenDlc { get; }
 
         /// <summary>
         /// Gets the operations related to identity management, such as identity verification and operations tied to user identification.
         /// </summary>
-        IIdentityOperations IdentityOperations { get; }
+        IIdentityOperations Identity { get; }
 
         /// <summary>
         /// Gets the operations related to phone number management.
         /// This includes various operations for managing phone numbers, such as purchasing, configuring, and releasing phone numbers.
         /// </summary>
-        IPhoneNumberOperations PhoneNumberOperations { get; }
+        IPhoneNumberOperations PhoneNumbers { get; }
 
         /// <summary>
         /// Gets the operations for programmable voice call commands.
         /// This includes operations such as dialing, answering, and managing calls programmatically via the Telnyx API.
         /// </summary>
-        ICallCommandsOperations CallCommandsOperations { get; }
+        ICallCommandsOperations CallCommands { get; }
     }
 }
