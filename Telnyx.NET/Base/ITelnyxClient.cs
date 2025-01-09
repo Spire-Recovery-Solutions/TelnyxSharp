dc45af8129a0fa6db4647f0946a783aa8fb31942
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-09 11:08:59 -0500

    reverted

--- Telnyx.NET/Base/ITelnyxClient.cs
+++ Telnyx.NET/Base/ITelnyxClient.cs
@@ -26,23 +26,23 @@ namespace Telnyx.NET.Base
         /// Gets the operations for 10DLC services.
         /// This includes managing 10DLC campaigns and configuring phone numbers for 10DLC services.
         /// </summary>
-        ITenDlcOperations TenDlc { get; }
+        ITenDlcOperations TenDlcOperations { get; }
 
         /// <summary>
         /// Gets the operations related to identity management, such as identity verification and operations tied to user identification.
         /// </summary>
-        IIdentityOperations Identity { get; }
+        IIdentityOperations IdentityOperations { get; }
 
         /// <summary>
         /// Gets the operations related to phone number management.
         /// This includes various operations for managing phone numbers, such as purchasing, configuring, and releasing phone numbers.
         /// </summary>
-        IPhoneNumberOperations PhoneNumber { get; }
+        IPhoneNumberOperations PhoneNumberOperations { get; }
 
         /// <summary>
         /// Gets the operations for programmable voice call commands.
         /// This includes operations such as dialing, answering, and managing calls programmatically via the Telnyx API.
         /// </summary>
-        ICallCommandsOperations CallCommands { get; }
+        ICallCommandsOperations CallCommandsOperations { get; }
     }
 }
