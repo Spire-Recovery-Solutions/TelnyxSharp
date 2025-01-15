0cde8efeeaf4faeeac05011bd5bcc6264973428b
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-15 10:42:52 -0500

    Pushed fix

--- Telnyx.NET/Enums/RejectCallCause.cs
+++ Telnyx.NET/Enums/RejectCallCause.cs
@@ -5,11 +5,25 @@ namespace Telnyx.NET.Enums
     /// <summary>
     /// Enumeration of possible causes for call rejection.
     /// </summary>
-    [JsonConverter(typeof(JsonStringEnumConverter<RejectCallCause>))]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RejectCallCause
     {
-        UNKNOWN,
-        CALL_REJECTED,
-        USER_BUSY
+        /// <summary>
+        /// Default unknown cause.
+        /// </summary>
+        [JsonPropertyName("UNKNOWN")]
+        Unknown,
+
+        /// <summary>
+        /// Call was rejected.
+        /// </summary>
+        [JsonPropertyName("CALL_REJECTED")]
+        CallRejected,
+
+        /// <summary>
+        /// User is busy.
+        /// </summary>
+        [JsonPropertyName("USER_BUSY")]
+        UserBusy
     }
 }
