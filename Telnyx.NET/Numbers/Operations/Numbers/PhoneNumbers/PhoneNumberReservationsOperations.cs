593c99c6f0dbb8f2954cf86fa873dfc5465ea1a6
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-20 14:00:03 -0500

    Added PhonenumberReservation section endpoints

--- Telnyx.NET/Numbers/Operations/Numbers/PhoneNumbers/PhoneNumberReservationsOperations.cs
+++ Telnyx.NET/Numbers/Operations/Numbers/PhoneNumbers/PhoneNumberReservationsOperations.cs
@@ -12,6 +12,20 @@ namespace Telnyx.NET.Numbers.Operations.Numbers.PhoneNumbers
     : BaseOperations(client, rateLimitRetryPolicy), IPhoneNumberReservationsOperations
     {
         /// <inheritdoc />
+        public async Task<ListNumberReservationsResponse> List(ListNumberReservationsRequest request, CancellationToken cancellationToken = default)
+        {
+            var req = new RestRequest("number_reservations")
+                .AddFilter("filter[status]", request.Status)
+                .AddFilter("filter[created_at][gt]", request.CreatedAfter)
+                .AddFilter("filter[created_at][lt]", request.CreatedBefore)
+                .AddFilter("filter[phone_numbers.phone_number]", request.PhoneNumber)
+                .AddFilter("filter[customer_reference]", request.CustomerReference)
+                .AddPagination(request.PageSize);
+
+            return await ExecuteAsync<ListNumberReservationsResponse>(req, cancellationToken);
+        }
+
+        /// <inheritdoc />
         public async Task<CreateNumberReservationResponse> Create(
             CreateNumberReservationRequest request, CancellationToken cancellationToken = default)
         {
@@ -20,5 +34,19 @@ namespace Telnyx.NET.Numbers.Operations.Numbers.PhoneNumbers
             return await ExecuteAsync<CreateNumberReservationResponse>(req, cancellationToken);
         }
 
+        /// <inheritdoc />
+        public async Task<GetNumberReservationResponse> Get(string numberReservationId, CancellationToken cancellationToken = default)
+        {
+            var req = new RestRequest($"number_reservations/{numberReservationId}");
+            return await ExecuteAsync<GetNumberReservationResponse>(req, cancellationToken);
+        }
+        
+        /// <inheritdoc />
+        public async Task<ExtendNumberReservationResponse> Extend(string numberReservationId, CancellationToken cancellationToken = default)
+        {
+            var request = new RestRequest($"number_reservations/{numberReservationId}/actions/extend", Method.Post);
+
+            return await ExecuteAsync<ExtendNumberReservationResponse>(request, cancellationToken);
+        }
     }
 }
