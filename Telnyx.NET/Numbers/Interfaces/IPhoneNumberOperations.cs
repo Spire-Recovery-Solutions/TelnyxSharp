765e8e149e9681b9fb9defc9edf7ee65b8bed5fd
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-20 12:30:43 -0500

    Refactoring acc to doc and Added PhoneNumberSearch section enpoints

---
+++ Telnyx.NET/Numbers/Interfaces/IPhoneNumberOperations.cs
@@ -0,0 +1,28 @@
+﻿namespace Telnyx.NET.Numbers.Interfaces
+{
+    /// <summary>
+    /// Provides operations related to phone number management, including searching, reserving, ordering, configuring, and retrieving phone numbers.
+    /// </summary>
+    public interface IPhoneNumberOperations : IDisposable
+    {
+        /// <summary>
+        /// Gets the operations related to phone number search, such as searching for available phone numbers.
+        /// </summary>
+        IPhoneNumberSearchOperations PhoneNumberSearch { get; }
+
+        /// <summary>
+        /// Gets the operations for phone number reservations, including creating and managing reserved numbers.
+        /// </summary>
+        IPhoneNumberReservationsOperations PhoneNumberReservations { get; }
+
+        /// <summary>
+        /// Gets the operations for managing phone number orders, including listing and creating orders.
+        /// </summary>
+        IPhoneNumberOrdersOperations PhoneNumberOrders { get; }
+
+        /// <summary>
+        /// Gets the operations for configuring phone numbers, including updating voice and other settings.
+        /// </summary>
+        IPhoneNumberConfigurationOperations PhoneNumberConfiguration { get; }
+    }
+}
