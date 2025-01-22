2b00bc39c9befa9664891f3840e592cd1ee13e68
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-22 12:06:26 -0500

    Implemented Regulatory Requirements section endpoints

---
+++ Telnyx.NET/Numbers/Models/PhoneNumbers/Requests/RegulatoryRequirements/GetRegulatoryRequirementsRequest.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Base;
+using Telnyx.NET.Enums;
+
+namespace Telnyx.NET.Numbers.Models.PhoneNumbers.Requests.RegulatoryRequirements
+{
+    /// <summary>
+    /// Represents a request for retrieving the regulatory requirements of a phone number.
+    /// This request includes filters such as the phone number, requirement group ID, 
+    /// country code, phone number type, and the action type.
+    /// </summary>
+    public class GetRegulatoryRequirementsRequest : ITelnyxRequest
+    {
+        /// <summary>
+        /// Gets or sets the phone number to check regulatory requirements for.
+        /// The phone number is used to filter the regulatory requirements associated with it.
+        /// </summary>
+        [JsonPropertyName("filter[phone_number]")]
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the requirement group ID to check regulatory requirements for.
+        /// This filter is used to narrow down the requirements to a specific group.
+        /// </summary>
+        [JsonPropertyName("filter[requirement_group_id]")]
+        public string? RequirementGroupId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country code (ISO2 format) to check regulatory requirements for.
+        /// This allows filtering by the geographical region or country associated with the phone number.
+        /// </summary>
+        [JsonPropertyName("filter[country_code]")]
+        public string? CountryCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of phone number to check regulatory requirements for.
+        /// Possible types include local, toll-free, mobile, national, and shared-cost.
+        /// </summary>
+        [JsonPropertyName("filter[phone_number_type]")]
+        public PhoneNumberType? PhoneNumberType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action type to check regulatory requirements for.
+        /// Possible actions include "ordering", "porting", or "action" to define the type of action for which requirements are being checked.
+        /// </summary>
+        [JsonPropertyName("filter[action]")]
+        public RequirementActionType? Action { get; set; }
+    }
+}
---
+++ Telnyx.NET/Numbers/Models/PhoneNumbers/Responses/RegulatoryRequirements/ListRegulatoryRequirementsResponse.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+using Telnyx.NET.Models;
+using Telnyx.NET.Numbers.Models.PhoneNumbers.Responses.PhoneNumberSearch;
+using System.Collections.Generic;
+
+namespace Telnyx.NET.Numbers.Models.PhoneNumbers.Responses.RegulatoryRequirements
+{
+    /// <summary>
+    /// Represents the response containing a list of regulatory requirements data for phone numbers.
+    /// Inherits from <see cref="TelnyxResponse{T}"/> where T is a list of <see cref="ListRegulatoryRequirementsData"/>.
+    /// </summary>
+    public class ListRegulatoryRequirementsResponse : TelnyxResponse<ListRegulatoryRequirementsData>
+    {
+    }
+
+    /// <summary>
+    /// Represents the data for regulatory requirements associated with a phone number, including region information, phone number type, and associated regulatory requirements.
+    /// </summary>
+    public class ListRegulatoryRequirementsData
+    {
+        /// <summary>
+        /// Gets or sets the phone number associated with the regulatory requirements.
+        /// </summary>
+        [JsonPropertyName("phone_number")]
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number type (e.g., local, toll-free, mobile).
+        /// </summary>
+        [JsonPropertyName("phone_number_type")]
+        public string? PhoneNumberType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region information related to the phone number, including geographic and regulatory details.
+        /// </summary>
+        [JsonPropertyName("region_information")]
+        public List<RegionInformation>? RegionInformation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the record type associated with the phone number (e.g., individual, business).
+        /// </summary>
+        [JsonPropertyName("record_type")]
+        public string? RecordType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of regulatory requirements associated with the phone number.
+        /// </summary>
+        [JsonPropertyName("regulatory_requirements")]
+        public List<RequirementDetail>? RegulatoryRequirements { get; set; }
+    }
+}
