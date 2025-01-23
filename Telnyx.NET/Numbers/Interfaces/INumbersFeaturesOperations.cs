f1aeb776f98c43f490ef8773e344112be54d89c1
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-23 14:32:28 -0500

    Implemented Advanced Number Orders & CSV downloads & Number Features section's endpoints

---
+++ Telnyx.NET/Numbers/Interfaces/INumbersFeaturesOperations.cs
@@ -0,0 +1,20 @@
+﻿using Telnyx.NET.Numbers.Models.PhoneNumbers.Requests.NumbersFeatures;
+using Telnyx.NET.Numbers.Models.PhoneNumbers.Responses.NumbersFeatures;
+
+namespace Telnyx.NET.Numbers.Interfaces
+{
+    /// <summary>
+    /// Provides operations for interacting with the Numbers Features API, 
+    /// allowing you to retrieve features associated with phone numbers.
+    /// </summary>
+    public interface INumbersFeaturesOperations
+    {
+        /// <summary>
+        /// Retrieves the features associated with the specified phone numbers.
+        /// </summary>
+        /// <param name="request">The request containing the phone numbers and other parameters to retrieve features for.</param>
+        /// <param name="cancellationToken">A cancellation token for controlling the request's lifetime.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the response with the numbers' features.</returns>
+        Task<GetNumbersFeaturesResponse> Get(GetNumbersFeaturesRequest request, CancellationToken cancellationToken = default);
+    }
+}
