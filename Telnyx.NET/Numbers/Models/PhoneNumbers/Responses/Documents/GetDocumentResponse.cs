74987b8bd2e4b71633fc3592599af49929279f5d
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-01-27 14:04:26 -0500

    Implemented PhoneNumber Porting and Documents section endpoints

---
+++ Telnyx.NET/Numbers/Models/PhoneNumbers/Responses/Documents/GetDocumentResponse.cs
@@ -0,0 +1,12 @@
+﻿using Telnyx.NET.Models;
+
+namespace Telnyx.NET.Numbers.Models.PhoneNumbers.Responses.Documents
+{
+    /// <summary>
+    /// Represents the response returned when retrieving a document associated with a phone number.
+    /// Inherits from TelnyxResponse to encapsulate the document data.
+    /// </summary>
+    public class GetDocumentResponse : TelnyxResponse<Document>
+    {
+    }
+}
